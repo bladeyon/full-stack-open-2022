4184ef7c51d3c38930f0a96c2a6d69db9508504a
yanglifeng <631676736@qq.com>
2022-12-07 11:35:15 +0800

    Practise 2.2. Show course total exercises

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -6,6 +6,8 @@ const Part = ({ part }) => (
   </p>
 );
 
+const Total = ({ sum }) => <h4>total of {sum} exercises </h4>;
+
 const Content = ({ parts }) => {
   console.log("Content");
   return (
@@ -19,10 +21,15 @@ const Content = ({ parts }) => {
 
 const Course = ({ course }) => {
   console.log("Course");
+  let sum = 0;
+  course.parts.forEach((part) => {
+    sum += part.exercises;
+  });
   return (
     <>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total sum={sum} />
     </>
   );
 };
