05af37f684ea01fef503b3c82b9bb6279b04e11b
yanglifeng <631676736@qq.com>
2022-12-07 11:28:09 +0800

    Init part2; Complete 2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,53 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  console.log("Content");
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  console.log("Course");
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  console.log("App");
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        { id: 1, name: "Fundamentals of React", exercises: 10 },
+        { id: 2, name: "Using props to pass data", exercises: 7 },
+        { id: 3, name: "State of a component", exercises: 14 }
+      ]
+    }
+  ];
+
+  return (
+    <>
+      {courses.map((course) => (
+        <Course key={course.id} course={course}></Course>
+      ))}
+    </>
+  );
+};
+
+export default App;
