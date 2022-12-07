74bf1eabd2e65e88ac65720f21af7a7bd49bb60b
yanglifeng <631676736@qq.com>
2022-12-07 11:50:17 +0800

    2.4: Multiple courses render

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h3>{course}</h3>;
 
 const Part = ({ part }) => (
   <p>
@@ -39,13 +39,23 @@ const App = () => {
       parts: [
         { id: 1, name: "Fundamentals of React", exercises: 10 },
         { id: 2, name: "Using props to pass data", exercises: 7 },
-        { id: 3, name: "State of a component", exercises: 14 }
+        { id: 3, name: "State of a component", exercises: 14 },
+        { id: 4, name: "Redux", exercises: 11 }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        { name: "Routing", exercises: 3, id: 1 },
+        { name: "Middlewares", exercises: 7, id: 2 }
       ]
     }
   ];
 
   return (
     <>
+      <h1>Web development</h1>
       {courses.map((course) => (
         <Course key={course.id} course={course}></Course>
       ))}
