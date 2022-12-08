72a2685c9292f51e7bb421946635c65c6043d6b8
yanglifeng <631676736@qq.com>
2022-12-08 13:52:02 +0800

    2.9: Filter person

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "hello note" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-123456", id: 1 },
+    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 }
+  ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState(persons);
 
   const handleSave = (e) => {
     e.preventDefault();
@@ -25,10 +32,22 @@ const App = () => {
     let value = e.target.value;
     setNewPhone(value);
   };
+  const handleQueryChange = (e) => {
+    let value = e.target.value;
+    console.log('query', value);
+    setQuery(value);
+    const newResult = persons.filter((person) =>
+      person.name.toLowerCase().includes(value.toLowerCase())
+    );
+    console.log(query, newResult);
+    setResult(newResult);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter person with <input value={query} onChange={handleQueryChange} />
+      <h2>Add new person</h2>
       <form onSubmit={handleSave}>
         name:
         <input name="text" value={newName} onChange={handleNameChange} />
@@ -38,8 +57,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name} {person.phone}</li>
+        {result.map((person) => (
+          <li key={person.name}>
+            {person.name} {person.phone}
+          </li>
         ))}
       </ul>
     </div>
