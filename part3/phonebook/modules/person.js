41794b76fe2089bf81e189709dd061222c231599
feng <631676736@qq.com>
2022-12-19 23:31:19 +0800

    3.20: Verify person's number

--- part3/phonebook/modules/person.js
+++ part3/phonebook/modules/person.js
@@ -12,7 +12,17 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    // required: true,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        const reg1 = /^\d{8,}/;
+        const reg2 = /^\d{2}-\d{5,}/;
+        const reg3 = /^\d{3}-\d{4,}/;
+        return reg1.test(v) || reg2.test(v) || reg3.test(v);
+      },
+      message: (props) => `${props.value} is not a valid number!`
+    }
   }
 });
 
