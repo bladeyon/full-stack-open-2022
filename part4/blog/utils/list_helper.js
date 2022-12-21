a527ee0715703973a622a57e637f2ad611635234
feng <631676736@qq.com>
2022-12-21 11:05:01 +0800

    4.5: Test favorite blog

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,22 @@
 const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, item) => sum + item.likes, 0);
+
+const favoriteBlog = (blogs) => {
+  let maxLikes = 0;
+  let favBlog = {};
+  blogs.forEach((b) => {
+    if (b.likes > maxLikes) {
+      maxLikes = b.likes;
+      const { title, author, likes } = b;
+      favBlog = { title, author, likes };
+    }
+  });
+  return favBlog;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
