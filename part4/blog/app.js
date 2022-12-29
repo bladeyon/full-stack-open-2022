52c2bea5869b55f9fa9d78f0c3efdda9dcc44882
feng <631676736@qq.com>
2022-12-29 11:00:18 +0800

    4.18: add login.js; use jsonwebtoken

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
 const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 
---
+++ part4/blog/controllers/login.js
@@ -0,0 +1,28 @@
+const loginRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+loginRouter.post('/', async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  const user = await User.findOne({ username });
+
+  const pwdCorrect = user === null ? false : await bcrypt.compare(password, user.password);
+
+  if (!(user && pwdCorrect)) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+
+  // generate token
+  const userForToken = {
+    username: user.username,
+    _id: user._id
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET_KEY);
+
+  res.status(200).json({ token, username: user.username, name: user.name });
+});
+
+module.exports = loginRouter;
